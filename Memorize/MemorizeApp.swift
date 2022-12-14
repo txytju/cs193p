94d4b123e61b1a37f73e1f888bbe01d06162c584
tianxingye <tianxingye@kuaishou.com>
2022-12-14 20:48:31 +0800

    programming assignment 1

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,18 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by tianxingye on 2022/12/1.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // 这个view描述了这个app应该长啥样
+            ContentView()
+        }
+    }
+}
